43a83fb841982b906766c31be0d125c1fc515aff
Tony <elkobrat@protonmail.com>
2021-01-01 20:53:22 +1000

    fix task_4

--- homework_#2/4_task.py
+++ homework_#2/4_task.py
@@ -4,5 +4,5 @@
   Если в слово длинное, выводить только первые 10 букв в слове.
 """
 line = input('Введите строку').split(' ')
-for index, word in enumerate(line):
-    print(index+1, word[:10])
+for index, word in enumerate(line, 1):
+    print(index, word[:10])
